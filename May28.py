554df912ab1efd4d0ddbeedeb08c5cd36400c96e
sungwoo woo <swpheus1@naver.com>
2018-05-28 21:18:04 +0900

    plot

---
+++ May28.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+img = mpimg.imread('stinkbug.png')
+
+
+
+
+# -------------------------------------------
+# print(img)
+# plt.imshow(img,cmap="nipy_spectral")
+# plt.hist(img.ravel(),bins=256,range=(0.0,1.0))
+# plt.colorbar()
+# plt.show()
+
+# print(img)
+# lum_img = img[:, :, 0]
+# plt.imshow(lum_img)
+# plt.imshow(img)
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+#
+#
+# plt.plot([1,2,3,4,5],[1,2,3,2,1],'k.')
+# plt.plot([1, 2, 3, 4,5], [5, 4, 3, 3,3], 'ro-')
+# plt.axis([0,6,0,6])
+# plt.show()
+
+
+
+# ---------------------------------
+# a = np.array([10,20,30,40])
+#
+# print(a*2)
+#
+# b=np.arange(20).reshape(2,5,2)
+#
+# print(b)
+
+# b = np.arange(20).reshape(2, 5, 2)
+# print(b)
+
+# a=np.array([10,20,30])
+# b=np.array([2,2,2])
+#
+# print(a+b)
+
+# b =np.ones(10)
+# print(b)
+
+# [0 0 100] # ruler
+# [100 0 100] #king
+# [100 0 0] #male
+# king - male =queen
+
+# a=np.sinc(3)
+# print(a)
+
+# a=np.arange(30).reshape(3,5,2)
+# #
+# # print(a[::2,::2,::2])
+# # print(a)
+
+
+
+
+
+
